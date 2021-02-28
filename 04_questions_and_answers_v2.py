78229dc521d7106d6809273e5769ba1479551f62
Woojin Jeon <jeonw70450@users.noreply.github.com>
2021-02-28 18:48:43 +1300

    Update 04_questions_and_answers_v2.py

--- 04_questions_and_answers_v2.py
+++ 04_questions_and_answers_v2.py
@@ -1,2 +1,22 @@
 import csv
+import random
 
+# Get list from csv and import it
+
+# name of csv file goes here...
+with open('country-list.csv', 'r') as f:
+
+    # make csv file into list
+    file = csv.reader(f)
+    next(f)
+    my_list = list(file)
+
+# choose an item from the main list, this item is itself a list
+question_ans = random.choice(my_list)
+
+# first item in small list
+question = question_ans[0]
+answer = question_ans[1]
+
+print("Question:Which Country has the capital {}?".format(question))
+print("Answer:{}".format(answer))
