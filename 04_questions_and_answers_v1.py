6181114c3dcebbdf9a4cf3e0dd858e0993dae0bf
Woojin Jeon <jeonw70450@users.noreply.github.com>
2021-02-28 18:48:45 +1300

    Update 04_questions_and_answers_v1.py

--- 04_questions_and_answers_v1.py
+++ 04_questions_and_answers_v1.py
@@ -1,6 +1,11 @@
-import pandas
+import csv
 
-colnames = ["Country", "Capital"]
-data = pandas.read_csv("country-list.csv", names=colnames)
 
-print(data)
+# name of csv file goes here...
+with open('country-list.csv', 'r') as f:
+
+    # make csv file into list
+    file = csv.reader(f)
+    my_list = list(file)
+
+print(my_list)
