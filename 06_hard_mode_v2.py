7f5a709b83dfc2067a0d0721a3339ec778934fca
Woojin Jeon <jeonw70450@users.noreply.github.com>
2021-03-03 14:54:01 +1300

    Create 06_hard_mode_v2.py

---
+++ 06_hard_mode_v2.py
@@ -0,0 +1,35 @@
+import csv
+import random
+import string
+
+
+
+# name of csv file goes here...
+with open('country-list.csv', 'r') as f:
+
+    # make csv file into list
+    file = csv.reader(f)
+    next(f)
+    my_list = list(file)
+
+score = 0
+# Loop the quiz 15 times
+for i in range(0,15):
+    # choose an item from the main list, this item is itself a list
+    question_ans = random.choice(my_list)
+
+    # first item in small list
+    question = question_ans[1]
+    answer = question_ans[0]
+
+    print("Question:Which Country has the capital {}?".format(question))
+    user_answer = input("Answer:")
+    if user_answer.casefold() == answer.casefold():
+        print("Correct!")
+        score += 1
+    else:
+        print("Incorrect, the correct answer is {}".format(answer))
+
+print("You managed to get {} right out of 15".format(score))
+
+
