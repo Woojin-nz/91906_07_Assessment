b51ed5b5403871b6de47769a2e84f570d68ef3b4
jeonw70450 <69278138+jeonw70450@users.noreply.github.com>
2021-03-11 11:22:34 +1300

    Update 07_easy_mode_v1.py

--- 07_easy_mode_v1.py
+++ 07_easy_mode_v1.py
@@ -31,19 +31,33 @@ class Game:
             next(f)
             my_list = list(file)
 
-        # choose an item from the main list, this item is itself a list
-        question_ans = random.choice(my_list)
-        print(question_ans)
-
         # Inital Score
         self.score = 0
 
         # Amounts of games played
         self.played = 0
 
-        # first item in small list
-        question = question_ans[1]
+        # chooses four different countries / capitals from the list
+        question_ans = random.choice(my_list)
+        yes = random.choice(my_list)
+        no= random.choice(my_list)
+        ok = random.choice(my_list)
+
+        # Defining variables for the capitals and countries,
+        # question is the capital in question
+        # self.answer is the correct answer
+        # incorrect[1,2,3] are the incorrect countries.
+        self.question = question_ans[1]
         self.answer = question_ans[0]
+        incorrect1 = yes[0]
+        incorrect2 = no[0]
+        incorrect3 = ok[0]
+        print(question_ans)
+
+        # I made the button_list a list so the list can be randomized so that the answer button locations is always
+        # different.
+        button_list = [self.answer, incorrect1, incorrect2, incorrect3]
+        random.shuffle(button_list)
 
         # GUI Setup
         self.game_box = Toplevel()
@@ -51,33 +65,38 @@ class Game:
         self.game_frame.grid(padx=10, pady=10)
 
         # Capital Label row 0
-        self.capital_label = Label(self.game_frame, text=question,
+        self.capital_label = Label(self.game_frame, text=self.question,
                                    font="Helvetica 15 bold")
         self.capital_label.grid(row=0)
 
         # Setup grid for answer buttons row 2
-        self.top_answers_frame = Frame(self.game_box)
+        self.top_answers_frame = Frame(self.game_box, width=50, height=50)
         self.top_answers_frame.grid(row=2, padx=5)
 
+        # width, wrap and height for buttons
+        wt=15
+        ht=3
+        wr=100
+
         # Top level answers buttons row 2.0
-        self.top_left_answer_button = Button(self.top_answers_frame, text="Top left",
-                                             font="Helvetica 10 bold", padx=5, pady=5,
+        self.top_left_answer_button = Button(self.top_answers_frame, text=button_list[0],
+                                             font="Helvetica 10 bold", padx=5, pady=5,width=wt,height=ht,wrap=wr,
                                              command=lambda: self.reveal_answer(0))
         self.top_left_answer_button.grid(column=0, row=0,padx=5,pady=5)
 
-        self.top_right_answer_button = Button(self.top_answers_frame, text="Top right",
-                                             font="Helvetica 10 bold", padx=5,pady=5,
+        self.top_right_answer_button = Button(self.top_answers_frame, text=button_list[1],
+                                             font="Helvetica 10 bold", padx=5,pady=5,width=wt,height=ht,wrap=wr,
                                              command=lambda: self.reveal_answer(1))
         self.top_right_answer_button.grid(column=1, row=0,padx=5,pady=5)
 
         # Bottom level answers buttons row 2.1
-        self.bottom_left_answer_button = Button(self.top_answers_frame, text="Bottom left",
-                                             font="Helvetica 10 bold", padx=5, pady=5,
+        self.bottom_left_answer_button = Button(self.top_answers_frame, text=button_list[2],
+                                             font="Helvetica 10 bold", padx=5, pady=5,width=wt,height=ht,wrap=wr,
                                              command=lambda: self.reveal_answer(2))
         self.bottom_left_answer_button.grid(column=0, row=1,padx=5,pady=5)
 
-        self.bottom_right_answer_button = Button(self.top_answers_frame, tex t="Bottom right",
-                                              font="Helvetica 10 bold", padx=5, pady=5,
+        self.bottom_right_answer_button = Button(self.top_answers_frame, text=button_list[3],
+                                              font="Helvetica 10 bold", padx=5, pady=5,width=wt,height=ht,wrap=wr,
                                               command=lambda: self.reveal_answer(3))
         self.bottom_right_answer_button.grid(column=1, row=1,padx=5,pady=5)
 
